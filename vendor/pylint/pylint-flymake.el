1d832b1fb777a746b580b65242bf623ba6e05ada
magcius <jstpierre@mecheye.net>
2010-01-17 00:01:18 -0500

    Oops, forgot to add pylint.

---
+++ vendor/pylint/pylint-flymake.el
@@ -0,0 +1,17 @@
+
+;; Configure flymake for python
+(load-library "flymake")
+(load-library "flymake-cursor")
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "epylint" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+;; Set as a minor mode for python
+(add-hook 'python-mode-hook '(lambda () (flymake-mode)))
